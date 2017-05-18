38989c18798c6c7a21fb0710cdbcd6b502732811
ComputerDude <playbaseball@gmail.com>
2017-05-18 18:39:24 -0400

    Changed a color code

--- src/us/plpl/uhc/References.java
+++ src/us/plpl/uhc/References.java
@@ -7,7 +7,7 @@ import us.plpl.uhc.utils.ColorManager;
 
 public class References {
 
-	public static String prefix = ColorManager.color("&f&l< &6&lU&2&lH&c&lC &f&l> ");
+	public static String prefix = ColorManager.color("&f&l< &6&lU&a&lH&c&lC &f&l> ");
 	public static ConsoleCommandSender console = Bukkit.getConsoleSender();
 	public static final String WORLD_NAME = "world";
 	
