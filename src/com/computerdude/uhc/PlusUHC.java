f7483616627f8524b821f5ebbf21ac1fea5ef45f
Justin Brubaker <justbru00@gmail.com>
2017-05-18 13:36:55 -0400

    Add commands to plugin.yml
    Fix some config stuff

--- src/com/computerdude/uhc/PlusUHC.java
+++ src/com/computerdude/uhc/PlusUHC.java
@@ -1,21 +1,39 @@
 package com.computerdude.uhc;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import static com.computerdude.uhc.References.console;
 
 public class PlusUHC extends JavaPlugin {
+	
+	private static PlusUHC plugin = null;
 
 	@Override
-	public void onEnable() {
-		if(this.getConfig().getBoolean("FirstStartup") == true) {
+	public void onEnable() {		
+		plugin = this; // Allow getting the plugin instance from other classes.
+		
+		/*if(this.getConfig().getBoolean("FirstStartup") == true) {
 			this.getConfig().set("FirstStartup", false);
 			this.saveConfig();
 			this.reloadConfig();
-		}
+		}*/ // Probably unneeded.
+		
+		saveDefaultConfig(); // Better method for saving config.
+		
+		console.sendMessage("Plus+ UHC enabled!");
 	}
 	
 	@Override
 	public void onDisable() {
 		
+		plugin = null; // Prevent a momory leak.
+	}
+	
+	/**
+	 * @author Justin Brubaker
+	 * @return Returns the current instance of the {@link PlusUHC} class.
+	 */
+	public static PlusUHC getInstance() {
+		return plugin;
 	}
 	
 }
--- src/com/computerdude/uhc/References.java
+++ src/com/computerdude/uhc/References.java
@@ -1,8 +1,13 @@
 package com.computerdude.uhc;
 
+import org.bukkit.Bukkit;
+import org.bukkit.command.ConsoleCommandSender;
+
 import com.computerdude.uhc.utils.ColorManager;
 
 public class References {
 
 	public static String prefix = ColorManager.color("&f&l< &6&lU&2&lH&c&lC &f&l> ");
+	public static ConsoleCommandSender console = Bukkit.getConsoleSender();
+	
 }
