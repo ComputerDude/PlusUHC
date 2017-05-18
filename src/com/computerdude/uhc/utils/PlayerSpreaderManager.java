46d09935e2880bbdb06867bde19cfee8669a5fe8
Justin Brubaker <justbru00@gmail.com>
2017-05-17 22:32:23 -0400

    Updated PlayerSpreader
    Now checks location info.

--- src/com/computerdude/uhc/utils/PlayerSpreaderManager.java
+++ src/com/computerdude/uhc/utils/PlayerSpreaderManager.java
@@ -4,6 +4,10 @@ import java.util.concurrent.ThreadLocalRandom;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 
 /**
@@ -14,30 +18,53 @@ import org.bukkit.entity.Player;
  *
  */
 public class PlayerSpreaderManager {
-	
+
 	private static String worldName = "world";
 
 	/**
-	 * Teleports one player to a random location in the world name in {@link worldName}
+	 * Teleports one player to a random location in the world name in
+	 * {@link worldName}
+	 * 
 	 * @param p
 	 */
 	public static void spreadPlayer(Player p) {
-		p.teleport(getRandomLocation());
+		Location loc;
+		while (true) {
+			loc = getRandomLocation();
+			Block block = loc.getBlock();			
+			Material type = block.getType();
+
+			boolean noPlayers = true;
+			
+			for (Entity e : Bukkit.getWorld(worldName).getNearbyEntities(loc, 100, 30, 100)) {
+				if (e.getType().equals(EntityType.PLAYER)) {
+					noPlayers = false;
+				}
+			}
+			
+			if (!noPlayers || type.equals(Material.LAVA) || type.equals(Material.STATIONARY_LAVA)) {
+				// Just let it pick a new spot.
+			} else {
+				break;
+			}
+		}
+		p.teleport(loc);
 	}
-	
+
 	/**
 	 * Random location finder.
+	 * 
 	 * @return
 	 */
 	private static Location getRandomLocation() {
 		Location loc = null;
-		
+
 		int x = ThreadLocalRandom.current().nextInt(-1000, 1001);
 		int z = ThreadLocalRandom.current().nextInt(-1000, 1001);
-		
+
 		loc = new Location(Bukkit.getWorld(worldName), x, Bukkit.getWorld(worldName).getHighestBlockYAt(x, z), z);
-		
+
 		return loc;
 	}
-	
+
 }
